85feee6ee09cdebc0a71a387bb18dbba83a137ec
Jason Data <jason.data@roadbytes.me>
2016-11-08 18:05:38 +0930

    Adding an Index Page

--- Gemfile
+++ Gemfile
@@ -4,3 +4,4 @@ ruby '2.3.0'
 
 gem 'sinatra', '~>1.4.7'
 gem 'sinatra-contrib'
+gem 'erubis'
--- file_cms.rb
+++ file_cms.rb
@@ -1,8 +1,15 @@
 require 'sinatra'
 require 'sinatra/reloader' if development?
+require 'tilt/erubis'
 
 set :server, 'webrick'
 
+root = File.expand_path("..", __FILE__)
+
 get '/' do
-  'Getting Started'
+  @files = Dir.glob(root + '/data/*').map do |path|
+    File.basename(path)
+  end
+
+  erb :index
 end
